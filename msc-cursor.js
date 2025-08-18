27f3b2468bf423762da5ebbc2f6a100f3fc4fe5a
Jamie Donaldson <jamie@outofbounds.studio>
2025-08-18 15:30:51 +0100

    chore: update version to 1.0.2 updated 18/08/2025 15.29

--- msc-cursor.js
+++ msc-cursor.js
@@ -1,5 +1,5 @@
 // msc.js - Metal Staircase Co. Website Scripts
-// Version: 1.0.2 updated 18/08/2025 15.24
+// Version: 1.0.2 updated 18/08/2025 15.29
 
 (function() {
     console.log('msc-cursor.js script loaded and executing!');
