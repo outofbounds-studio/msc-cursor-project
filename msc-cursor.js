51d3b2d83a85b520fcb87743a984f80e4278a4a8
Jamie Donaldson <jamie@outofbounds.studio>
2025-08-19 16:47:03 +0100

    fix: prevent form validation interference with lightbox functionality

--- msc-cursor.js
+++ msc-cursor.js
@@ -1,5 +1,5 @@
 // msc.js - Metal Staircase Co. Website Scripts
-// Version: 1.0.2 updated 19/08/2025 16.08
+// Version: 1.0.2 updated 19/08/2025 16.46
 
 (function() {
     console.log('msc-cursor.js script loaded and executing!');
